1fc1b3ebc3d0de8f843e3a4f2e6a4a84efbedd20
feliciano <felicianojrsousa@gmail.com>
2017-06-30 08:08:07 -0300

    nova correção

--- app/src/main/java/br/com/mabelli/bijoux/AppActivity.java
+++ app/src/main/java/br/com/mabelli/bijoux/AppActivity.java
@@ -15,41 +15,35 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.firebase.database.Query;
 
 
 /**
  * Created by feliciano on 21/02/17.
  */
 
-public class AppActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, AppContract.View {
+public class AppActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DatabaseReference mDatabase;
-    private AppContract.ActionListener listener;
-    private FireAdapter adapter;
-    private RecyclerView recyclerView;
+    private FirebaseAdapter adapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        listener = new AppPresenter(this, mDatabase);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -63,16 +57,27 @@ public class AppActivity extends AppCompatActivity implements NavigationView.OnN
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        Intent intent = new Intent(this, FcmRegisterService.class);
+        startService(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        init();
+    }
+
+    private void init() {
+        final RecyclerView recyclerView;
         recyclerView = (RecyclerView) findViewById(R.id.recyclerViewMenu);
         recyclerView.setHasFixedSize(true);
-        adapter = new FireAdapter();
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         StaggeredGridLayoutManager st = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(st);
+        adapter = new FirebaseAdapter(Product.class, R.layout.itens_list, ProductRecycler.class,
+                mDatabase.child("product/").orderByChild("type").equalTo("necklace").getRef()
+        );
         recyclerView.setAdapter(adapter);
-
-        Intent intent = new Intent(this, FcmRegisterService.class);
-        startService(intent);
     }
 
     @Override
@@ -91,33 +96,15 @@ public class AppActivity extends AppCompatActivity implements NavigationView.OnN
         }
     }
 
-    public final MyListener mylistener = new MyListener() {
-        @Override
-        public void onMyClick(Product product) {
-            listener.saveFavorite(product);
-            Toast.makeText(AppActivity.this, "OK", Toast.LENGTH_SHORT).show();
-        }
-    };
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    private int dpToPx(int dp) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
-                getResources().getDisplayMetrics());
-    }
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            //adapter = new FireAdapter();
-            listener.getnecklace();
+            init();
+            //listener.getnecklace();
         } else if (id == R.id.nav_gallery) {
-            listener.getErraing();
+            // listener.getErraing();
 
         } else if (id == R.id.nav_slideshow) {
             startActivity(new Intent(this, AuthActivity.class));
@@ -134,96 +121,60 @@ public class AppActivity extends AppCompatActivity implements NavigationView.OnN
     }
 
     @Override
-    public void addPath(Product product) {
-        adapter.addPath(product);
+    protected void onDestroy() {
+        super.onDestroy();
+        adapter.cleanup();
     }
 
-    @Override
-    public void clearView() {
-        recyclerView.removeAllViews();
+    public String getUid() {
+        return FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
-    @Override
-    public void setAction(AppContract.ActionListener listener) {
-        this.listener = listener;
-    }
+    public class FirebaseAdapter extends FirebaseRecyclerAdapter<Product, ProductRecycler> {
 
-    public class FireAdapter extends RecyclerView.Adapter<FireAdapter.ItemRecycler> {
-
-        private List<Product> mPostPaths;
-
-        public FireAdapter() {
-            mPostPaths = new ArrayList<Product>();
+        /**
+         * @param modelClass      Firebase will marshall the data at a location into an instance of a class that you provide
+         * @param modelLayout     This is the layout used to represent a single item in the list. You will be responsible for populating an
+         *                        instance of the corresponding view with the data from an instance of modelClass.
+         * @param viewHolderClass The class that hold references to all sub-views in an instance modelLayout.
+         * @param ref             The Firebase location to watch for data changes. Can also be a slice of a location, using some
+         *                        combination of {@code limit()}, {@code startAt()}, and {@code endAt()}.
+         */
+        public FirebaseAdapter(Class<Product> modelClass, int modelLayout, Class<ProductRecycler> viewHolderClass, Query ref) {
+            super(modelClass, modelLayout, viewHolderClass, ref);
         }
 
         @Override
-        public ItemRecycler onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(AppActivity.this);
-            final View view = inflater.inflate(R.layout.itens_list, parent, false);
-            return new ItemRecycler(view, mylistener);
+        protected void populateViewHolder(ProductRecycler viewHolder, final Product product, int position) {
+            product.uid = getRef(position).getKey();
+            viewHolder.priceView.setText(product.title);
+            Glide.with(AppActivity.this).load(product.url).crossFade().centerCrop().into(viewHolder.imgView);
+            viewHolder.btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    mDatabase.child("favorite/").child(getUid()).child(product.uid)
+                            .setValue(true);
+                    Toast.makeText(AppActivity.this, "Clickou", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
-
-        @Override
-        public void onBindViewHolder(final ItemRecycler holder, int position) {
-            final Product product = mPostPaths.get(position);
-            //  holder.titleView.setText(product.getPrice());
-            holder.priceView.setText(product.title);
-            Glide.with(AppActivity.this).load(product.url).crossFade().centerCrop().into(holder.imgView);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mPostPaths.size();
-        }
-
-        public void addPath(Product path) {
-            mPostPaths.add(path);
-            notifyItemInserted(mPostPaths.size());
-        }
-
-        public void addPaths(List<Product> paths) {
-            int startIndex = mPostPaths.size();
-            mPostPaths.addAll(paths);
-            notifyItemRangeInserted(startIndex, mPostPaths.size());
-        }
-
-        public Product getItem(int position) {
-            return mPostPaths.get(position);
-        }
-
-
-        public class ItemRecycler extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-            public final TextView titleView;
-            public final ImageView imgView;
-            public final TextView priceView;
-            public final AppCompatImageButton btn;
-            //  View view;
-            private final MyListener myListener;
-
-            public ItemRecycler(View itemView, MyListener myListener) {
-                super(itemView);
-                // view = itemView;
-                this.myListener = myListener;
-                titleView = (TextView) itemView.findViewById(R.id.name);
-                imgView = (ImageView) itemView.findViewById(R.id.comment_photo);
-                priceView = (TextView) itemView.findViewById(R.id.artist);
-                btn = (AppCompatImageButton) itemView.findViewById(R.id.btnfavorite);
-                btn.setOnClickListener(this);
-            }
-
-            @Override
-            public void onClick(View v) {
-                int position = getAdapterPosition();
-                Log.i("TAG",String.valueOf(position));
-                Product product = getItem(position);
-                myListener.onMyClick(product);
-            }
-        }
-
     }
 
-    public interface MyListener {
-        void onMyClick(Product product);
+    public static class ProductRecycler extends RecyclerView.ViewHolder {
+
+        public final TextView titleView;
+        public final ImageView imgView;
+        public final TextView priceView;
+        public final AppCompatImageButton btn;
+
+        public ProductRecycler(View itemView) {
+            super(itemView);
+            titleView = (TextView) itemView.findViewById(R.id.name);
+            imgView = (ImageView) itemView.findViewById(R.id.comment_photo);
+            priceView = (TextView) itemView.findViewById(R.id.artist);
+            btn = (AppCompatImageButton) itemView.findViewById(R.id.btnfavorite);
+        }
+
     }
 }
